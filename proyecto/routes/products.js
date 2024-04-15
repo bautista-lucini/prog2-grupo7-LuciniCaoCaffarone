7af46fde4f006e86baffca6072aa34f35f9f3d92
bautista-lucini <blucini@udesa.edu.ar>
2024-04-15 15:17:25 -0300

    arreglando rutas

--- proyecto/routes/products.js
+++ proyecto/routes/products.js
@@ -4,3 +4,5 @@ const productsController = require('../controllers/productsController.js');
 
 router.get('/', productsController.index);
 router.get('/product-add', productsController.add);
+
+module.exports = router;
