05f2a8a4b9f0ab330aa336102538092abd50847e
bautista-lucini <blucinie@udesa.edu.ar>
2024-04-17 21:47:29 -0300

    products

--- proyecto/controllers/productsController.js
+++ proyecto/controllers/productsController.js
@@ -2,7 +2,7 @@ const db = require('../db/medias.js');
 
 const productsController = {
     index: function(req, res) {
-        res.render('product',{title: "Detalle del producto", productos: db.lista_productos});
+        res.render('product',{title: "Detalle del producto", productos: db.lista_productos,  productId: req.params.id});
     },
     add: function(req, res) {
         res.render('product-add', {title: "Añadir un producto", usuario: db.usuario});
