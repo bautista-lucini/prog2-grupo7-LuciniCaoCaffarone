454587be068f9147be2a87f9afca3f6db7fcb3f4
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-06-11 13:34:00 -0300

    session

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products')
+const session = require('express-session');
 
 
 var app = express();
