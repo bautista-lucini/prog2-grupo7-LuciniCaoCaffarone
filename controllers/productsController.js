a488f4970beff9ae50deedbce2170b7529bc094f
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-06-12 17:18:12 -0300

    Punto 5, Buscador

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,56 +1,72 @@
 const db = require('../database/models');
 
 //let indexController = {
-   // index: function(req, res){}
-      //  db.Movie.findAll()
-       // .then(function (data){
-        //   return res.send(data)
-       // })
-       // .catch(function(error){
-     //       console.log(error);
-       // })
-       
-    // info para poder hacer controladores con sequelice 
+// index: function(req, res){}
+//  db.Movie.findAll()
+// .then(function (data){
+//   return res.send(data)
+// })
+// .catch(function(error){
+//       console.log(error);
+// })
+
+// info para poder hacer controladores con sequelice 
 const productsController = {
 
-     //chequear desde esta linea hasta la 53
+  //chequear desde esta linea hasta la 53
 
-     
-    products : function (req,res) {
-        let idMedia = req.params.id;
 
-        let filtrado = {
-          include: [
-            {association: "duenio"},
-            {association: "comentarios"}
-          ]
-        }
+  products: function (req, res) {
+    let idMedia = req.params.id;
 
-    
+    let filtrado = {
+      include: [
+        { association: "duenio" },
+        { association: "comentarios" }
+      ]
+    }
+
+
 
     db.Producto.findByPk(idMedia, filtrado)
-    .then(function(result) {
-              //return res.render("detalleMovies", {movie: result})
-              return res.send(result)
+      .then(function (result) {
+        //return res.render("detalleMovies", {movie: result})
+        return res.send(result)
       })
-      .catch(function(error) {
+      .catch(function (error) {
         return console.log(error);;
-    });
+      });
 
-
-
-    
-   
-
-   
   },
 
 
-    index: function(req, res) {
-        res.render('product',{title: "Detalle del producto", productos: db.lista_productos,  productId: req.params.id});
-    },
-    add: function(req, res) {
-        res.render('product-add', {info:db, usuarios: db.lista_usuarios, productos: db.lista_productos, username: req.params.username });
+  search: function (req, res) {
+    res.render('product', { title: "Detalle del producto", productos: db.lista_productos, productId: req.params.id });
+  },
+  add: function (req, res) {
+    res.render('product-add', { info: db, usuarios: db.lista_usuarios, productos: db.lista_productos, username: req.params.username });
+  },
+
+
+
+  showOne: function(req, res) {
+    
+  let querys = req.query.search;
+   //return res.send(querys)
+
+    let filtrado = {
+      where: [{nombre_producto: querys}]
     }
+
+    db.Producto.findOne(filtrado)
+    .then(function(result) {
+      return res.send(result);
+    }).catch(function(error) {
+      return console.log(error);;
+    });
+
+    
+
+  }
 }
 module.exports = productsController;
--- routes/products.js
+++ routes/products.js
@@ -3,8 +3,11 @@ var router = express.Router();
 const productsController = require('../controllers/productsController.js');
 
 //router.get("/product", productsController.product)
-router.get('/add/:username', productsController.add);
-router.get('/:id', productsController.index); //metodo index no esta bien
+
+router.get('/id/:id', productsController.products); //metodo index no esta bien
+router.get('/add', productsController.add);
+router.get('/search-results', productsController.showOne);
+
 
 module.exports = router;
 
