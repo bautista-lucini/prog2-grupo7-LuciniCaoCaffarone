2fb7a09f455a43d3656e38d46c73c0c0b3e1f3f4
lcaffarone <lcaffarone@udesa.edu.ar>
2024-06-19 16:05:55 -0300

    termina ruta form

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const productsController = require('../controllers/productsController.js');
+const { body } = require('express-validator');
+
 
 //router.get("/product", productsController.product)
 
@@ -11,6 +13,13 @@ router.get('/search-results', productsController.showOne);
 
 router.get('/create', productsController.createForm);
 router.post('/create', productsController.create);
+
+router.get('/create', productsController.add);
+router.post('/create', [
+  body('name').notEmpty().withMessage('El nombre es obligatorio'),
+  body('description').notEmpty().withMessage('La descripción es obligatoria'),
+  body('image').notEmpty().withMessage('La imagen es obligatoria')
+], productsController.create);
 
 
 module.exports = router;
