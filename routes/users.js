ba2537c4907afd06f78f2906c5e589c7c89a156c
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-06-21 19:42:44 -0300

    validations punto 11

--- routes/users.js
+++ routes/users.js
@@ -71,6 +71,38 @@ let registerValidations = [
     body('fotoPerfil')
 ]
 
+let editValidations = [
+
+  body("name")
+  .notEmpty().bail(),
+body('email') 
+  .notEmpty() 
+  .isEmail()
+  .custom(function(value){ 
+       return db.Usuario.findOne({
+         where: { email: value }, 
+       })
+           .then(function(usuario){
+              if(usuario){
+               throw new Error('El email ingresado ya existe.');
+              }
+           })
+}),
+body('usuario')
+    .notEmpty().withMessage('Debes completar el nombre de usuario').bail()
+    .isLength({ min: 5 }).withMessage('El nombre de usuario debe ser más largo'), 
+body('contraseña')
+    .bail()
+    .isLength({ min: 4 }).withMessage('La contraseña debe contener al menos 4 caracteres'),
+body('fechaNacimiento')
+    .isDate().withMessage('Debe ingresar una fecha en el formato YYYY/MM/DD').bail(),
+body("nroDocumento")
+   .isNumeric().withMessage("Este campo debe ser completado solo con números").bail(),
+body('fotoPerfil')
+
+]
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -86,7 +118,7 @@ router.post('/logout', usersController.logout);
 
 router.get('/edit/:id', usersController.profileEdit);
 router.get('/profile/:id', usersController.profile);
-router.post('/update/:id', usersController.updateUser)
+router.post('/update/:id',editValidations, usersController.updateUser);
 
 
 router.get('/check-session', (req, res) => {
