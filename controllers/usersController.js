c76ccf1f0ae9c3aa183c3c007e8e26e749606725
antoniocaoo <acaolavignolle@udesa.edu.ar>
2024-06-23 18:30:20 -0300

    arreglo validatiosn

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,17 +3,17 @@ const db = require('../database/models');
 const usuarios = db.Usuario;
 const productos = db.Producto;
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const usersController = {
-    login: function(req, res) {
+    login: function (req, res) {
         if (req.session.userId) {
             return res.redirect('/users/profile/' + req.session.userId);
         } else {
             return res.render('login');
         }
     },
-    postLogin: function(req, res) {
+    postLogin: function (req, res) {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.render('login', { errors: errors.mapped(), old: req.body });
@@ -21,25 +21,26 @@ const usersController = {
             const user = req.user;
             req.session.userId = user.id;
             if (req.body.recordarme) {
-                res.cookie('usuarioRecordado', user.id, { maxAge: 1000 * 60 * 60 * 24 * 7 }); 
+                res.cookie('usuarioRecordado', user.id, { maxAge: 1000 * 60 * 60 * 24 * 7 });
             }
             res.redirect('/users/profile/' + req.session.user.id);
-        } },
-    logout: function(req, res){
+        }
+    },
+    logout: function (req, res) {
         req.session.destroy();
         console.log(req.session);
         res.clearCookie('usuarioRecordado');
         console.log(req.cookies.usuarioRecordado);
         return res.redirect('/');
     },
-    register: function(req, res) {
+    register: function (req, res) {
         if (req.session.userId) {
             return res.redirect('/users/profile/' + req.session.userId);
         } else {
             return res.render('register');
         }
     },
-    store: function(req, res){
+    store: function (req, res) {
         let formulario = req.body;
         let user = {
             nombre: formulario.name,
@@ -47,61 +48,66 @@ const usersController = {
             usuario: formulario.usuario,
             contraseña: bcrypt.hashSync(formulario.contraseña, 10),
             fecha: formulario.fechaNacimiento,
-            dni: formulario.nroDocumento, 
+            dni: formulario.nroDocumento,
             foto_perfil: formulario.fotoPerfil,
             createdAt: new Date()
         };
         let errors = validationResult(req);
-        if (errors.isEmpty()){
+        if (errors.isEmpty()) {
             db.Usuario.create(user)
-            .then(function(result){
-                res.redirect('/users/profile/' + result.id)
-            })
+                .then(function (result) {
+                    res.redirect('/users/profile/' + result.id)
+                })
         } else {
-            res.render('register', {errors: errors.mapped(), old: req.body})
+            res.render('register', { errors: errors.mapped(), old: req.body })
         }
     },
-    profile: function(req, res){
+    profile: function (req, res) {
         let id = req.params.id;
         let criterio = {
             include: [
-                {association: "medias"},
-                {association: "comentarios"},
-                
+                { association: "medias" },
+                { association: "comentarios" },
+
             ],
-            order: [["createdAt","DESC"]],
+            order: [["createdAt", "DESC"]],
         };
-        usuarios.findByPk(id,criterio)
-        .then(function(usuario) {
-            res.render("profile", { info: usuario, user: res.locals.user });
-          })
+        usuarios.findByPk(id, criterio)
+            .then(function (usuario) {
+                res.render("profile", { info: usuario, user: res.locals.user });
+            })
     },
-    profileEdit: function(req, res){
+    profileEdit: function (req, res) {
         let id = req.params.id;
         //console.log(id)
         usuarios.findByPk(id)
-        .then(function(usuario){
-           // res.send(usuario)
-           res.render('profile-edit', { usuario: usuario });
-        });
-       
-        let errors = validationResult(req);
-        if (errors.isEmpty()){
-            db.Usuario.create(user) // no se deberia crear, sino updatear, 
-            .then(function(result){
-                res.redirect('/users/profile/' + result.id)
-            })
-        } else {
-           // res.render('register', {errors: errors.mapped(), old: req.body})  Esta linea esta mal, pq nos lleva a register y no a editar mi perfil
-        }
-    
+            .then(function (usuario) {
+                // res.send(usuario)
+                res.render('profile-edit', { usuario: usuario });
+            });
+
+
+
     },
-    updateUser: function(req,res){
+    updateUser: function (req, res) {
         //return res.send(req.body)
-        if(req.params.id == req.session.userId){
+        let errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            db.Usuario.update(user) 
+                .then(function (result) {
+                    res.redirect('/users/profile/' + result.id)
+                })
+        } else {
+            // res.render('register', {errors: errors.mapped(), old: req.body})  Esta linea esta mal, pq nos lleva a register y no a editar mi perfil
+        }
+
+
+
+        if (req.params.id == req.session.userId) {
             let usuario = {}
-            if(req.body.contraseña.length>0){
-                 usuario = {
+            if (req.body.contraseña.length > 0) {
+                usuario = {
                     nombre: req.body.name,
                     usuario: req.body.usuario,
                     email: req.body.email,
@@ -109,8 +115,8 @@ const usersController = {
                     fecha: req.body.fechaNacimiento,
                     dni: req.body.nroDocumento,
                 }
-            }else{
-                 usuario = {
+            } else {
+                usuario = {
                     nombre: req.body.name,
                     usuario: req.body.usuario,
                     email: req.body.email,
@@ -118,21 +124,22 @@ const usersController = {
                     dni: req.body.nroDocumento,
                 }
             }
-            
-            usuarios.update(usuario,{
-                where:{
-                    id : req.params.id
+
+            usuarios.update(usuario, {
+                where: {
+                    id: req.params.id
                 }
             })
-            .then(function(){
-                res.redirect('/users/profile/' + req.params.id)
-            })
-        }else{
+                .then(function () {
+                    res.redirect('/users/profile/' + req.params.id)
+                })
+        } else {
             res.redirect('/users/profile/' + req.session.userId)
         }
-        
+
 
     }
+
 
 
 };
